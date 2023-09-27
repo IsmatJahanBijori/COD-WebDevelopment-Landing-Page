7553b7b68bd6d64799f3fc2925f6fdabf459c651
IsmatJahanBijori <ismatjahanbijori@gmail.com>
2023-09-28 01:27:09 +0600

    blog added

--- script/script.js
+++ script/script.js
@@ -1,39 +1,6 @@
 
-// // script.js
-// const slides = document.querySelectorAll('.slide');
-// const prevButton = document.getElementById('prev');
-// const nextButton = document.getElementById('next');
 
-// let currentIndex = 0;
-
-// function showSlide(index) {
-//     // slides.forEach((slide, i) => {
-//     //     slide.style.transform = `translateX(${100 * (i - index)}%)`;
-//     // });
-
-//     slides.forEach((slide, i) => {
-//         if (i === index) {
-//             slide.style.opacity = 1;
-//         } else {
-//             slide.style.opacity = 0;
-//         }
-//     });
-// }
-
-// prevButton.addEventListener('click', () => {
-//     currentIndex = (currentIndex - 1 + slides.length) % slides.length;
-//     showSlide(currentIndex);
-// });
-
-// nextButton.addEventListener('click', () => {
-//     currentIndex = (currentIndex + 1) % slides.length;
-//     showSlide(currentIndex);
-// });
-
-// // Show the first slide initially
-// showSlide(currentIndex);
-
-var slide_index = 1;
+let slide_index = 1;
 displaySlides(slide_index);
 function nextSlide(n) {
     displaySlides(slide_index += n);
@@ -42,12 +9,29 @@ function currentSlide(n) {
     displaySlides(slide_index = n);
 }
 function displaySlides(n) {
-    var i;
-    var slides = document.getElementsByClassName("showSlide");
-    if (n > slides.length) { slide_index = 1 }
-    if (n < 1) { slide_index = slides.length }
+    let i;
+    let slides = document.getElementsByClassName("slide-info");
+    if (n > slides.length) 
+    { slide_index = 1 }
+    if (n < 1)
+     { slide_index = slides.length }
+
     for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
     }
     slides[slide_index - 1].style.display = "block";
 }
+
+
+
+
+const accordionButtons = document.querySelectorAll('.accordion-btn');
+const accordionContents = document.querySelectorAll('.accordion-details');
+
+// Add click event listeners to buttons
+accordionButtons.forEach((button, index) => {
+    button.addEventListener('click', () => {
+        // Toggle the active class to show/hide content
+        accordionContents[index].classList.toggle('active');
+    });
+});
